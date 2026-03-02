f50d75df79c807e493b85bd67b0ba7822cf54df9
esingh16 <esingh16@umd.edu>
2026-03-02 18:00:44 -0500

    Creating App component for the main application.

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './App.css';
+
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+            </Routes>
+        </Router>
+    );
+}
